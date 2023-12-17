019d35a6187b36eb816fe348546dadc5f783b804
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2023-12-17 22:57:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function fibonacci(num) {
 // your code here
 	let a=0;
 	let b=1;
-	if(num==0)
-		return a
 	if(num==1)
-		return b
+		return 0
+	if(num==2)
+		return 1
 	let c;
 	for (let index = 2; index <=num; index++) {
 		c=a+b;
