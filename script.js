d43dae11a483d653b990240739a098be764c3a2c
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2023-12-18 00:37:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
-function fibonacci(num) {
-// your code here
-	let a=0;
-	let b=1;
-	if(num==1)
-		return 0
-	if(num==2)
-		return 1
-	let c;
-	for (let index = 2; index <=num; index++) {
-		c=a+b;
-		a=b;
-		b=c;
-	}
-	return c;
+// function fibonacci(num) {
+// // your code here
+// 	let a=0;
+// 	let b=1;
+// 	if(num==1){
+// 		return 0;
+// 	}
+// 	if(num==2){
+// 		return 1;
+// 	}
+// 	let c;
+// 	for (let index = 2; index <=num; index++) {
+// 		c=a+b;
+// 		a=b;
+// 		b=c;
+// 	}
+// 	return c;
+// }
+function fibonacci(n) {
+  // initialize the first two terms
+  let a = 0;
+  let b = 1;
+  // check if n is 0 or 1 and return the corresponding term
+  if (n === 0) {
+    return a;
+  }
+  if (n === 1) {
+    return b;
+  }
+  // use a for loop to iterate from the third term to the nth term
+  for (let i = 2; i <= n; i++) {
+    // calculate the next term as the sum of the previous two terms
+    let c = a + b;
+    // update the previous two terms
+    a = b;
+    b = c;
+  }
+  // return the nth term
+  return b;
 }
-
 module.exports = fibonacci;
